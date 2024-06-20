8a6214977a3a1ee506a2eb59bfdb8279d5c161ab
Raminder Singh <raminder.singh@spirent.com>
2024-06-20 15:07:36 -0700

    some change

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,16 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/", handlerFunc)
+	//the above 2 lines OR below 1 line.
 	http.HandleFunc("/", handlerFunc)
+	fmt.Println("logic simulated by sleeping for 60 seconds")
+	time.Sleep(1 * time.Minute)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil) //nil means it passes to defaultservemux which is what is used by HandleFunc,
+	// instead of you creating a mux in which case you put mux in place of nil
+	if err != nil {
+		panic(err)
+	}
 }
