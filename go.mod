253728b94e4b72250b81e99e4a0f70a938266006
Raminder Singh <raminder.singh@spirent.com>
2024-06-21 16:04:26 -0700

    changes with chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/raminderis/lenslocked
 
 go 1.21.7
+
+require github.com/go-chi/chi/v5 v5.0.14 // indirect
--- main.go
+++ main.go
@@ -3,23 +3,80 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to Rami's awesome site!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+	<h2>
+		Hi this work is awesome
+	</h2>
+	`)
+
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+	<h2>
+		My Contact:
+	</h2>
+	<p> Email me at <a href=\"mailto:jon@live.com\">jon@live.com</a.</p>>
+	`)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li>
+			<b>Is there a free version</b>
+			Yes
+		</li>
+		<li>
+			<b>Is there a free version</b>
+			Yes
+		</li>
+		<li>
+			<b>Is there a free version</b>
+			EMail us at <a href=\"mailto:rami@liv.com\">rami@liv.com</a>
+		</li>
+	</ul>
+	`)
+}
+
+// type Router struct{}
+
+//	func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//		fmt.Println("goning  now on 3000")
+//		switch r.URL.Path {
+//		case "/":
+//			homeHandler(w, r)
+//		case "/faq":
+//			faqHandler(w, r)
+//		case "/contact":
+//			contactHandler(w, r)
+//		default:
+//			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+//		}
+//	}
 func main() {
-	//mux := http.NewServeMux()
-	//mux.HandleFunc("/", handlerFunc)
-	//the above 2 lines OR below 1 line.
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("logic simulated by sleeping for 3 seconds")
-	time.Sleep(3 * time.Second)
-	fmt.Println("Starting the server on :3000...")
-	err := http.ListenAndServe(":3000", nil) //nil means it passes to defaultservemux which is what is used by HandleFunc,
-	// instead of you creating a mux in which case you put mux in place of nil
+	r := chi.NewRouter()
+	//r.Use(middleware.Logger)
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("welcome"))
+	// })
+	r.Get("/", homeHandler)
+	r.Get("/contact", homeHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	})
+	//var router Router
+	fmt.Println("listening now on 3000")
+	err := http.ListenAndServe("127.0.0.1:3000", r)
 	if err != nil {
 		panic(err)
 	}
