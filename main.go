6b1969e49eca434df8783352d26f5504662e9409
Raminder Singh <raminder.singh@spirent.com>
2024-06-23 09:42:29 -0700

    added common executetemplate func

--- main.go
+++ main.go
@@ -11,10 +11,9 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpath := filepath.Join("templates", "home.gohtml")
-	t, err := template.ParseFiles(tpath)
+	t, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("Parsing template error: %v", err)
 		http.Error(w, "There was error parsing the template.", http.StatusInternalServerError)
@@ -28,19 +27,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tpath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tpath)
+}
+
 func usersHandler(w http.ResponseWriter, r *http.Request) {
 	userID := chi.URLParam(r, "userID")
 	w.Write([]byte(fmt.Sprintf("hi %v", userID)))
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `
-	<h2>
-		My Contact:
-	</h2>
-	<p> Email me at <a href=\"mailto:jon@live.com\">jon@live.com</a.</p>>
-	`)
+	tpath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tpath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,7 +66,7 @@ func main() {
 	r := chi.NewRouter()
 	r.With(middleware.Logger).Get("/users/{userID}", usersHandler)
 	r.Get("/", homeHandler)
-	r.Get("/contact", homeHandler)
+	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
