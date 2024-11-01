ae4c9d236ccf6b944d818b02c70b0ef71915f82e
Raminder Singh <rasingh@calm2wl6p4fww.lan>
2024-11-01 12:43:13 -0700

    first files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pro
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	//set status code as 200 ok
+	w.WriteHeader(http.StatusOK)
+	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting the server on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
