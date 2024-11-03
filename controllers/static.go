a8e752ea11aada15eb2fe201d15d095dcbfdf193
Raminder Singh <raminderis@live.com>
2024-11-03 10:33:14 -0800

    some more change

---
+++ controllers/static.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+	"pro/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
--- main.go
+++ main.go
@@ -3,46 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
-	"html/template"
-	"log"
 	"net/http"
-	"path/filepath"
+	"pro/controllers"
+	"pro/templates"
+	"pro/views"
 )
 
-// parse and return template
-// execute the given template
-func executeTemplate(w http.ResponseWriter, tplPath string, data interface{}) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(tplPath)
-	if err != nil {
-		log.Printf("Error parsing template: %v", err)
-		http.Error(w, "There was error parsing the template", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, data)
-	if err != nil {
-		log.Printf("Error executing template: %v", err)
-		http.Error(w, "There was error executing the template", http.StatusInternalServerError)
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "home.gohtml"), nil)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "contact.gohtml"), nil)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.gohtml"), nil)
-}
-
 func main() {
+
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
+
+	r.Get("/contact", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+
+	r.Get("/faq", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	})
