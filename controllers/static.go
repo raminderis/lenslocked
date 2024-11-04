dc34c052fe1d85de8d6b165d0d48b0e722c73b6f
Raminder Singh <raminderis@live.com>
2024-11-03 17:20:47 -0800

    add to signup branch

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 	"pro/views"
 )
@@ -8,5 +9,32 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQHandler(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "is there a free version?",
+			Answer:   "yes free for 30 days.",
+		},
+		{
+			Question: "is there a abc version?",
+			Answer:   "yes abc for 30 days.",
+		},
+		{
+			Question: "is there a gfd version?",
+			Answer:   "yes gfd for 30 days.",
+		},
+		{
+			Question: "what your email?",
+			Answer:   "my email is <a href='mailto:raminderis@live.co'>chmicanga@live.com</a>",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
--- main.go
+++ main.go
@@ -14,13 +14,16 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQHandler(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+
+	r.Get("/signup", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
