73b1c1c071c9eb2ababa8fd57f05855269347046
Raminder <raminderis@live.com>
2025-12-25 19:51:03 -0800

    tailwind started

--- main.go
+++ main.go
@@ -11,11 +11,6 @@ import (
 	"github.com/raminderis/lenslocked/views"
 )
 
-func rawPathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Path: "+r.URL.Path+"</h1>")
-	fmt.Fprint(w, "<h1>Raw Path: "+r.URL.RawPath+"</h1>")
-}
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -39,16 +34,15 @@ func main() {
 		},
 	}
 
-	t := views.Must(views.ParseFS(templates.FS, "home.gohtml", "layout-parts.gohtml"))
+	t := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
 	r.Get("/", controllers.StaticHandler(t, data))
 
-	t = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "layout-parts.gohtml"))
+	t = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(t, data))
 
-	t = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "layout-parts.gohtml"))
+	t = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(t, data))
 
-	r.Get("/dog/*", rawPathHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	})
